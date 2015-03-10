e30c49d8db98ec896e18f6ab414ce8f64ed0fb1e
rajbala <rajbala@gmail.com>
2015-03-10 11:29:05 -0400

    Added visudo.sh to automate adding user to sudoers

--- configuration.py
+++ configuration.py
@@ -46,6 +46,16 @@ South
 redis
 psycopg2 '''
 
+visudo = '''
+#!/bin/sh
+if [ -z "$1" ]; then
+  echo "Starting up visudo with this script as first parameter"
+  export EDITOR=$0 && sudo -E visudo
+else
+  echo "Changing sudoers"
+  echo "%s ALL=(ALL:ALL) ALL" >> $1
+fi ''' % (NEW_OPERATING_SYSTEM_USER)
+
 nginx_conf = '''
 # the upstream component nginx needs to connect to
 upstream django {
